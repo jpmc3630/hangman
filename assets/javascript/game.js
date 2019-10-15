509d12bc492f5fddc955f46ed23ab22fa1278337
James McGlone <jamespmcglone@gmail.com>
2019-10-15 14:07:39 +0800

    Splashscreen, new genres added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -104,23 +104,62 @@ let hangManParts = [
 
 
 // generate dictionary
-let dictionary = ["techno", "drum and bass", "house", "hip hop", "dubstep", "psytrance", "trance"];
-var currentSongURL = "https://music.ishkur.com/music/Detroit%20Techno%20-%20(1993)%20Sebastian%20S.%20-%20Etheral.mp3";
+let dictionary = [
+"techno", 
+"drum and bass", 
+"house", 
+"hip hop", 
+"dubstep", 
+"psytrance", 
+"trance", 
+"disco", 
+"breaks", 
+"french house", 
+"rave", 
+"minimal techno", 
+"deep house", 
+"trip hop", 
+"goa trance", 
+"chiptune", 
+"breakcore", 
+"industrial"
+];
+
 var songURLs = [
-  "https://music.ishkur.com/music/Detroit%20Techno%20-%20(1993)%20Sebastian%20S.%20-%20Etheral.mp3",
+"https://music.ishkur.com/music/Detroit%20Techno%20-%20(1993)%20Sebastian%20S.%20-%20Etheral.mp3",
 "https://music.ishkur.com/music/Pendulum%20-%20(2009)%20Shock%20One%20-%20Polygon.mp3",
 "https://music.ishkur.com/music/Chicago%20House%20-%20(1987)%20The%20Children%20-%20Freedom%20(Factory%20mix).mp3",
 "https://music.ishkur.com/music/Conscious%20Rap%20-%20(2005)%20GZA%20&%20Ras%20Kass%20-%20Lyrical%20Swords.mp3",
 "https://music.ishkur.com/music/Dubstep%20-%20(2011)%20J-Kenzo%20-%20The%20Roteks.mp3",
 "https://music.ishkur.com/music/Full%20On%20-%20(2006)%20Mahamudra%20vs%20Dan%20Label%20-%20Electro%20Something.mp3",
-"https://music.ishkur.com/music/Trance%20-%20(1990)%20Liaisons%20D.%20-%20He%20Chilled%20Out.mp3"]
+"https://music.ishkur.com/music/Trance%20-%20(1990)%20Liaisons%20D.%20-%20He%20Chilled%20Out.mp3",
+"https://music.ishkur.com/music/Disco%20House%20-%20(2000)%20C-Mos%20-%206-2%20Young.mp3",
+"https://music.ishkur.com/music/Breaks%20-%20(1990)%20The%20Scientist%20-%20The%20Bee.mp3",
+"https://music.ishkur.com/music/French%20House%20-%20(1995)%20Daft%20Punk%20-%20Da%20Funk.mp3",
+"https://music.ishkur.com/music/Rave%20-%20(1992)%20DJ%20Seduction%20-%20Hardcore%20Heaven.mp3",
+"https://music.ishkur.com/music/Minimal%20Techno%20-%20(2001)%20Mikael%20Stavostrand%20-%20Repl.mp3",
+"https://music.ishkur.com/music/Euro%20Deep%20House%20-%20(2008)%20Liapin%20-%20Redskin%20(Agnes%20mix).mp3",
+"https://music.ishkur.com/music/Trip%20Hop%20-%20(1996)%20Amon%20Tobin%20-%20Daytrip.mp3",
+"https://music.ishkur.com/music/Goa%20Trance%20-%20(1995)%20Polyploid%20-%20Coaster%20Prefix.mp3",
+"https://music.ishkur.com/music/Chiptune%20-%20(2016)%20Donutshoes%20-%20Highway%20Lottery.mp3",
+"https://music.ishkur.com/music/Breakcore%20-%20(2005)%20Kgb%20Kid%20-%20Any%20Bwoy%20Test.mp3",
+"https://music.ishkur.com/music/Industrial%20-%20(2013)%20Gesaffelstein%20-%20Hate%20Or%20Glory.mp3"
+];
    
+
+var currentSongURL = "https://music.ishkur.com/music/Detroit%20Techno%20-%20(1993)%20Sebastian%20S.%20-%20Etheral.mp3";
+
 var audioElement = document.createElement("audio");
 var muted = true;
-let WonWords = [];
+
 let GameOver = false;
 
 let Word = [];
+let WonWords = [];
+
+let GuessCycler = [];
+
+
 let GuessedLetters = [];
 let IncorrectGuesses = [];
 let GuessesRemaining = 10;
@@ -133,8 +172,23 @@ function allLetter(inputtxt) {
 
 function NewWord() {
     
-        // generate random number for dictionary index
-        let WordIndex = Math.floor((Math.random() * dictionary.length));
+        // generate random number for dictionary index and check word hasn't been guessed yet
+        let WordIndex = 0;
+        let CheckWord = "";
+
+        //loop checks random word against GuessCycler (recent words) 
+        do {
+          WordIndex = Math.floor((Math.random() * dictionary.length));
+          
+          // CheckWord = (dictionary[WordIndex].split('').join(' ').toUpperCase());
+          // to be used when game is made completable, for now dictionary recycles
+
+        }
+        while (GuessCycler.includes(WordIndex));
+        
+        // GuessCycler prevents the same WORD coming up within 5 more attempts
+        GuessCycler.push(WordIndex);
+        if (GuessCycler.length > 5) {GuessCycler.shift()};
         
         // grab randomized word from dictionary
         let WordStr = dictionary[WordIndex];
@@ -206,11 +260,13 @@ function printScreen() {
     } else {
         
         WonWords.push(PrintedWord.join(" "));
+
         let WordText = "";
         if (WonWords.length === 1) {WordText = "genre";} else {WordText = "genres";};
         WonWordsDiv.innerHTML = "You have mastered " + WonWords.length + " " + WordText + "<br>" + WonWords.join("<br>");
         GameStatus.innerHTML = "You Win. Press SPACE for new game.";
         GameOver = true;
+
 
         
     };
@@ -294,8 +350,15 @@ window.onload = function() {
     audioElement.setAttribute("src", currentSongURL);
     
     document.getElementById("unmute-button").addEventListener("click", unMute);
+    document.getElementById("letsgo").addEventListener("click", LetsGoFunction);
 
 
+function LetsGoFunction() {
+  NewGame(); 
+  muted = false;
+  document.getElementById("splash").style.display= 'none';
+
+};
  
 
 function unMute() {
@@ -304,12 +367,14 @@ function unMute() {
   if (muted) {
     audioElement.play();
     muted = false;
-    document.getElementById("unmute-button").classList.remove("btn-default");
-    document.getElementById("unmute-button").classList.add("btn-danger");  
+    document.getElementById("unmute-button").classList.remove("btn-danger");
+    document.getElementById("unmute-button").classList.add("btn-default");
+
   } else {audioElement.pause();
       muted = true;
-      document.getElementById("unmute-button").classList.remove("btn-danger");
-      document.getElementById("unmute-button").classList.add("btn-default");  
+ 
+      document.getElementById("unmute-button").classList.remove("btn-default");
+      document.getElementById("unmute-button").classList.add("btn-danger"); 
   };};
 
 
